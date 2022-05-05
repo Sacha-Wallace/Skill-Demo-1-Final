c233790649a25d52007a893a2d1f5889d51d5096
Sacha-Wallace <sawallace@ucsd.edu>
2022-05-04 18:28:30 -0700

    Fixed test case

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SkillDemoTest {
+    @Test
+    public void testMethod(){
+        assertEquals(1, SkillDemoMethod.Subtract(3, 2)); // false
+    }
+}

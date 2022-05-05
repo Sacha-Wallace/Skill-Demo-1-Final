c171a5d5f927cad934126107a14ff1ac833cad44
Sacha-Wallace <sawallace@ucsd.edu>
2022-05-04 18:34:30 -0700

    f

--- SkillDemoMethod.java
+++ SkillDemoMethod.java
@@ -1,5 +1,6 @@
 public class SkillDemoMethod{
-    public static int Subtract(){
-        
+    //Subtracts num2 from num1
+    public static int Subtract(int num1, int num2){
+        return num2 - num1;
     }
 }
